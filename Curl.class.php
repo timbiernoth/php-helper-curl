545b111296a80cd49911fe74a99a265a098ec4a4
Tim Biernoth <60134914+timbiernoth@users.noreply.github.com>
2020-01-22 10:46:55 +0100

    First commit

---
+++ Curl.class.php
@@ -0,0 +1,49 @@
+<?php
+
+class Curl
+{
+    public function getContent($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_MAXREDIRS, 5);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+        curl_setopt($curl ,CURLOPT_USERAGENT,  'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+        $code = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $code;
+    }
+
+    public function getPostContent($url, $data)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $data_string = json_encode($data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'content-type:application/json;charset=utf-8',
+            'content-length:' . strlen($data_string),
+        ]);
+
+        $result = curl_exec($ch);
+
+        return $result;
+    }
+}
